ff5c1bee80050ca83e392322f683f88d0eeacbac
OsamaManzoor <osamamanzoor123@outlook.com>
2023-07-24 18:02:53 +0500

    Temp Screen added.

--- app/src/main/java/com/example/appinterface/activities/HomeActivity.java
+++ app/src/main/java/com/example/appinterface/activities/HomeActivity.java
@@ -3,33 +3,114 @@ package com.example.appinterface.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.appinterface.MainActivity;
 import com.example.appinterface.R;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class HomeActivity extends AppCompatActivity {
 
     RelativeLayout tempBtn;
+    TextView weatherDegree, weatherDesc;
+    ImageView weatherIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        // binding views
+        weatherIcon = findViewById(R.id.weather_icon);
+        weatherDegree = findViewById(R.id.weather_degree);
+        weatherDesc = findViewById(R.id.weather_desc);
         tempBtn = findViewById(R.id.temp_sc);
-        tempBtn.setOnClickListener(new View.OnClickListener() {
+
+        // click listeners
+        tempBtn.setOnClickListener(new onClickList());
+
+        //
+        getWeatherData();
+    }
+
+    private void getWeatherData() {
+
+//    https://api.openweathermap.org/data/2.5/weather?q=rawalpindi&appid=7787dcf9ad85f0b42c05890322de4e56
+         final String url = "https://api.openweathermap.org/data/2.5/weather";
+         final String api = "7787dcf9ad85f0b42c05890322de4e56";
+         String city = "rawalpindi";
+
+
+        final String hitURL = url + "?q=" + city + "&appid=" + api;
+
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, hitURL, null, new Response.Listener<JSONObject>() {
             @Override
-            public void onClick(View view) {
+            public void onResponse(JSONObject response) {
+
+                try {
+                    JSONObject object = response.getJSONObject("main");
+                    String temperature = object.getString("temp");
+                    temperature = temperature.substring(0,3);
+                    int temp = Integer.parseInt(temperature);
+                    temp -= 273;
+                    weatherDegree.setText("" + temp + "°C");
+
+                    JSONArray array = response.getJSONArray("weather");
+                    JSONObject obj = array.getJSONObject(0);
+                    String desc = obj.getString("description");
+                    weatherDesc.setText(desc.substring(0, 1).toUpperCase() + desc.substring(1));
+
+                    desc = obj.getString("icon");
+                    String url = "https://openweathermap.org/img/w/" + desc + ".png";
+
+                    Picasso.get().load(url).resize(35,35).into(weatherIcon);
+                } catch (JSONException e) {
+                    Toast.makeText(getApplicationContext(), e.toString().trim(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.toString().trim(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        queue.add(request);
+    }
+
+    private class onClickList implements View.OnClickListener{
+
+        @Override
+        public void onClick(View view) {
+            int viewID = view.getId();
+
+            if(viewID == R.id.temp_sc){
                 Intent next = new Intent(getApplicationContext(), TemperatureActivity.class);
                 startActivity(next);
             }
-        });
-
+        }
     }
-
 
 }
--- app/src/main/java/com/example/appinterface/activities/TemperatureActivity.java
+++ app/src/main/java/com/example/appinterface/activities/TemperatureActivity.java
@@ -3,15 +3,30 @@ package com.example.appinterface.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.appinterface.R;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class TemperatureActivity extends AppCompatActivity {
 
-    private ProgressBar tempProgressBar, humidProgressBar;
-    private TextView tempProgressText, humidProgressText;
+    private ProgressBar tempProgressBar;
+    private TextView tempProgressText, sensorTemp, sensorHumid;
+    private TextView weatherFeelsLike, weatherStatus, weatherCity;
+    private ImageView weatherIcon;
     private int ROOM_TEMP = 38;
     private int ROOM_HUMIDITY = 65;
 
@@ -23,10 +38,17 @@ public class TemperatureActivity extends AppCompatActivity {
 
         tempProgressBar = findViewById(R.id.temp_progress_bar);
         tempProgressText = findViewById(R.id.temp_progress_text);
-        humidProgressBar = findViewById(R.id.humid_progress_bar);
-        humidProgressText = findViewById(R.id.humid_progress_text);
+        sensorTemp = findViewById(R.id.sensor_temp);
+        sensorHumid = findViewById(R.id.sensor_humid);
+
+        weatherFeelsLike = findViewById(R.id.weather_feels_degree);
+        weatherStatus = findViewById(R.id.weather_status);
+        weatherCity = findViewById(R.id.weather_city);
+        weatherIcon = findViewById(R.id.weather_img);
+
 
         set_temp_humidity();
+        getWeatherData();
 
     }
 
@@ -35,7 +57,57 @@ public class TemperatureActivity extends AppCompatActivity {
         tempProgressText.setText("" + ROOM_TEMP + "ºC");
         tempProgressBar.setProgress(ROOM_TEMP);
 
-        humidProgressText.setText("" + ROOM_HUMIDITY + "%");
-        humidProgressBar.setProgress(ROOM_HUMIDITY);
+        sensorTemp.setText(""+ ROOM_TEMP + "°C");
+        sensorHumid.setText(""+ ROOM_HUMIDITY + "%");
+    }
+
+
+    private void getWeatherData() {
+
+//    https://api.openweathermap.org/data/2.5/weather?q=rawalpindi&appid=7787dcf9ad85f0b42c05890322de4e56
+        final String url = "https://api.openweathermap.org/data/2.5/weather";
+        final String api = "7787dcf9ad85f0b42c05890322de4e56";
+        String city = "islamabad";
+
+
+        final String hitURL = url + "?q=" + city + "&appid=" + api;
+
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, hitURL, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+                try {
+                    JSONObject object = response.getJSONObject("main");
+                    String feels_like = object.getString("feels_like");
+                    feels_like = feels_like.substring(0,3);
+                    int temp = Integer.parseInt(feels_like);
+                    temp -= 273;    // kelvin to celcius
+                    weatherFeelsLike.setText("" + temp + "°C");
+
+                    JSONArray array = response.getJSONArray("weather");
+                    JSONObject obj = array.getJSONObject(0);
+                    String desc = obj.getString("description");
+                    weatherStatus.setText(desc.substring(0, 1).toUpperCase() + desc.substring(1));
+
+                    desc = obj.getString("icon");
+                    String url = "https://openweathermap.org/img/w/" + desc + ".png";
+                    Picasso.get().load(url).resize(35,30).into(weatherIcon);
+
+                    String cityName = response.getString("name");
+                    weatherCity.setText(cityName);
+
+                } catch (JSONException e) {
+                    Toast.makeText(getApplicationContext(), e.toString().trim(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.toString().trim(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        queue.add(request);
     }
 }
